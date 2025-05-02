34d9c9dd80c54ccbd6d0d9111864762133cd5302
sofiamb31 <sofiamadrigal2303@gmail.com>
2025-05-02 15:03:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ def encode_image(image_file):
 
 st.set_page_config(page_title="Analisis de imagen", layout="centered", initial_sidebar_state="collapsed")
 # Streamlit page setup
-st.title("Análisis de Imagen:🤖🏞️")
+st.title("Análisis de Imagen:🧐🧩")
 ke = st.text_input('Ingresa tu Clave')
 os.environ['OPENAI_API_KEY'] = ke
 
